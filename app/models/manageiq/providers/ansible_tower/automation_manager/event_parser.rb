898f65ffdc0a874e392b5b328f5f8ee3b97d775d
Greg Blomquist <gblomqui@redhat.com>
2017-02-24 20:31:55 -0500

    Share AnsibleTower Eventing
    (transferred from ManageIQ/manageiq@df0a9e5969bc9aae715d89e61c5fe15b35fa849e)

--- app/models/manageiq/providers/ansible_tower/automation_manager/event_parser.rb
+++ app/models/manageiq/providers/ansible_tower/automation_manager/event_parser.rb
@@ -1,12 +1,11 @@
 module ManageIQ::Providers::AnsibleTower::AutomationManager::EventParser
-  def self.event_to_hash(event, ems_id)
-    {
-      :event_type => "ansible_tower_#{event.operation}",
-      :source     => "ANSIBLE_TOWER",
-      :message    => event.changes.to_s,
-      :timestamp  => event.timestamp,
-      :full_data  => event.to_h,
-      :ems_id     => ems_id
-    }
+  extend ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::EventParser
+
+  def self.event_type
+    "ansible_tower"
+  end
+
+  def self.source
+    "ANSIBLE_TOWER"
   end
 end
