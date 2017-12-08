e1361aa692d4e0b5948280ee6c8e0a65622b75ee
mkanoor <mkanoor@redhat.com>
2017-12-08 17:25:37 -0500

    Added supported_catalog_types

--- spec/models/manageiq/providers/ansible_tower/automation_manager_spec.rb
+++ spec/models/manageiq/providers/ansible_tower/automation_manager_spec.rb
@@ -1,3 +1,11 @@
 describe ManageIQ::Providers::AnsibleTower::AutomationManager do
   it_behaves_like 'ansible automation_manager'
+
+  context 'catalog types' do
+    let(:ems) { FactoryGirl.create(:automation_manager_ansible_tower) }
+
+    it "#supported_catalog_types" do
+      expect(ems.supported_catalog_types).to eq(%w(generic_ansible_tower))
+    end
+  end
 end
