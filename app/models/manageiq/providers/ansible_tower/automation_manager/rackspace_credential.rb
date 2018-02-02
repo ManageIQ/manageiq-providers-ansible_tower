5ac4dcd0278d06b34cea9c0bedd976a2a866bf89
James Wong <webpotent@gmail.com>
2018-02-01 22:24:37 -0500

    Dropping azure classic and rackspace credential - following Tower 3.2
    https://bugzilla.redhat.com/show_bug.cgi?id=1542182

--- app/models/manageiq/providers/ansible_tower/automation_manager/rackspace_credential.rb
+++
@@ -1,3 +0,0 @@
-class ManageIQ::Providers::AnsibleTower::AutomationManager::RackspaceCredential < ManageIQ::Providers::AnsibleTower::AutomationManager::CloudCredential
-  include ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::RackspaceCredential
-end
