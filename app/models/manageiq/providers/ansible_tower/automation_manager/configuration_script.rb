15f5ecdd982c5c4fbf4b792bb34b46c63284df5d
Lucy Fu <lufu@redhat.com>
2018-06-29 12:16:17 -0400

    Add supports_limit? for Ansible job/workflow templates.
    https://bugzilla.redhat.com/show_bug.cgi?id=1590975

--- app/models/manageiq/providers/ansible_tower/automation_manager/configuration_script.rb
+++ app/models/manageiq/providers/ansible_tower/automation_manager/configuration_script.rb
@@ -7,4 +7,8 @@ class ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript 
   def self.display_name(number = 1)
     n_('Job Template (Ansible Tower)', 'Job Templates (Ansible Tower)', number)
   end
+
+  def supports_limit?
+    true
+  end
 end
--- app/models/manageiq/providers/ansible_tower/automation_manager/configuration_workflow.rb
+++ app/models/manageiq/providers/ansible_tower/automation_manager/configuration_workflow.rb
@@ -1,3 +1,7 @@
 class ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationWorkflow < ManageIQ::Providers::ExternalAutomationManager::ConfigurationScript
   include ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::ConfigurationWorkflow
+
+  def supports_limit?
+    false
+  end
 end
