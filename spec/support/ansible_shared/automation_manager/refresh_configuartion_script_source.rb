4d38b0e8c31d3ba23de2562200b2db7c28d521d5
Tomas Coufal <tcoufal@redhat.com>
2018-07-23 18:27:45 +0200

    Revert "Save projects’ last update stdout" on targeted VCR cassette
    Reverted "Save projects’ last update stdout" f7c616e8767752551fefb78b5e75bb1fcf58b418
    on `refresh_configuartion_script_source.rb` VCR cassette

--- spec/support/ansible_shared/automation_manager/refresh_configuartion_script_source.rb
+++ spec/support/ansible_shared/automation_manager/refresh_configuartion_script_source.rb
@@ -20,10 +20,10 @@ shared_examples_for "refresh configuration_script_source" do |ansible_provider, 
     configuration_script_source = FactoryGirl.create(:"#{ems_type}_configuration_script_source",
                                                      :authentication => credential,
                                                      :manager        => automation_manager,
-                                                     :manager_ref    => 325)
+                                                     :manager_ref    => 472)
     configuration_script_source.configuration_script_payloads.create!(:manager_ref => '2b_rm', :name => '2b_rm')
     configuration_script_source_other = FactoryGirl.create(:"#{ems_type}_configuration_script_source",
-                                                           :manager_ref => 322,
+                                                           :manager_ref => 5,
                                                            :manager     => automation_manager,
                                                            :name        => 'Dont touch this')
 
@@ -31,7 +31,7 @@ shared_examples_for "refresh configuration_script_source" do |ansible_provider, 
     stub_const("ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::ConfigurationScriptSource::REFRESH_ON_TOWER_SLEEP", 0.seconds)
 
     # this is to check if a project will be updated on tower
-    last_project_update = Time.zone.parse("2018-05-23 09:29:41.7849700 +0000") - 1.minute
+    last_project_update = Time.zone.parse("2017-04-10 20:50:11.429285000 +0000") - 1.minute
 
     Spec::Support::VcrHelper.with_cassette_library_dir(ManageIQ::Providers::AnsibleTower::Engine.root.join("spec/vcr_cassettes")) do
       2.times do
@@ -48,23 +48,23 @@ shared_examples_for "refresh configuration_script_source" do |ansible_provider, 
           expect(last_updated).to be >= last_project_update
           last_project_update = last_updated
 
-          expect(configuration_script_source.name).to eq("hello_repo")
+          expect(configuration_script_source.name).to eq("targeted_refresh")
           expect(configuration_script_source.last_updated_on).to eq(last_updated)
-          expect(ConfigurationScriptPayload.count).to eq(61)
+          expect(ConfigurationScriptPayload.count).to eq(81)
           expect(ConfigurationScriptPayload.where(:name => '2b_rm')).to be_empty
-          expect(configuration_script_source.configuration_script_payloads.count).to eq(61)
+          expect(configuration_script_source.configuration_script_payloads.count).to eq(81)
           expect(
             configuration_script_source.configuration_script_payloads.where(
-              :name => "jboss-standalone/demo-aws-launch.yml"
+              :name => "test/utils/docker/httptester/httptester.yml"
             ).count
           ).to eq(1)
-          expect(configuration_script_source.authentication.name).to eq('hello_scm_cred')
+          expect(configuration_script_source.authentication.name).to eq('db-github')
           expect(credential.reload).to eq(credential)
 
           expect(configuration_script_source_other.name).to eq("Dont touch this")
         end
         # check if a playbook will be added back in on the second run
-        configuration_script_source.configuration_script_payloads.where(:name => "jboss-standalone/demo-aws-launch.yml").destroy_all
+        configuration_script_source.configuration_script_payloads.where(:name => "test/utils/docker/httptester/httptester.yml").destroy_all
       end
     end
   end
