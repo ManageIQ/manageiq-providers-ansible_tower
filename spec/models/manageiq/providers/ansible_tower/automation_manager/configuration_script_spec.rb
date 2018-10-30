d9a4dfb9b3d458bf2d9ac777897e677efb6bd90a
Lucy Fu <lufu@redhat.com>
2018-10-30 14:23:27 -0400

    Add Ansible template runner.
    https://bugzilla.redhat.com/show_bug.cgi?id=1591797

--- spec/models/manageiq/providers/ansible_tower/automation_manager/configuration_script_spec.rb
+++ spec/models/manageiq/providers/ansible_tower/automation_manager/configuration_script_spec.rb
@@ -2,10 +2,20 @@ describe ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScri
   let(:provider_with_authentication)       { FactoryBot.create(:provider_ansible_tower, :with_authentication) }
   let(:manager_with_authentication)        { provider_with_authentication.managers.first }
   let(:manager_with_configuration_scripts) { FactoryBot.create(:automation_manager_ansible_tower, :provider, :configuration_script) }
+  subject { FactoryBot.create(:ansible_configuration_script, :manager => manager_with_configuration_scripts) }
 
   it_behaves_like 'ansible configuration_script'
 
   it 'designates orchestration stack type' do
     expect(described_class.stack_type).to eq('Job')
+  end
+
+  describe '#run_with_miq_job' do
+    it 'delegates request to template runner' do
+      double_return = double(:signal => nil, :miq_task => double(:id => 'tid'))
+      expect(ManageIQ::Providers::AnsibleTower::AutomationManager::TemplateRunner)
+        .to receive(:create_job).with(hash_including(:ansible_template_id => subject.id, :userid => 'system')).and_return(double_return)
+      expect(subject.run_with_miq_job({})).to eq('tid')
+    end
   end
 end
