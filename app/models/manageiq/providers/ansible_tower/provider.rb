45042c57c20f95e75043f0b4a2200a369352b0cc
Jason Frey <jfrey@redhat.com>
2019-09-30 18:15:47 -0400

    Add DDF based verify_credentials
    Part of https://github.com/ManageIQ/manageiq/issues/18818

--- app/models/manageiq/providers/ansible_tower/provider.rb
+++ app/models/manageiq/providers/ansible_tower/provider.rb
@@ -8,6 +8,88 @@ class ManageIQ::Providers::AnsibleTower::Provider < ::Provider
   validates :name, :presence => true, :uniqueness => true
   validates :url,  :presence => true
 
+  PARAMS_FOR_CREATE = {
+    :title  => "Configure Ansible Tower",
+    :fields => [
+      {
+        :component  => "text-field",
+        :name       => "endpoints.default.base_url",
+        :label      => "URL",
+        :isRequired => true,
+        :validate   => [{:type => "required-validator"}]
+      },
+      {
+        :component  => "text-field",
+        :name       => "endpoints.default.username",
+        :label      => "Username",
+        :isRequired => true,
+        :validate   => [{:type => "required-validator"}]
+      },
+      {
+        :component  => "text-field",
+        :name       => "endpoints.default.password",
+        :label      => "Password",
+        :type       => "password",
+        :isRequired => true,
+        :validate   => [{:type => "required-validator"}]
+      },
+      {
+        :component => "checkbox",
+        :name      => "endpoints.default.verify_ssl",
+        :label     => "Verify SSL"
+      }
+    ]
+  }.freeze
+
+  def self.params_for_create
+    PARAMS_FOR_CREATE
+  end
+
+  # Verify Credentials
+  # args:
+  # {
+  #   "endpoints" => {
+  #     "default" => {
+  #       "base_url"   => "",
+  #       "username"   => "",
+  #       "password"   => "",
+  #       "verify_ssl" => ""
+  #     }
+  #   }
+  # }
+  def self.verify_credentials(args)
+    default_endpoint = args.dig("endpoints", "default")
+
+    base_url, username, password, verify_ssl = default_endpoint&.values_at(
+      "base_url", "username", "password", "verify_ssl"
+    )
+    base_url   = adjust_url(base_url)
+    verify_ssl = verify_ssl ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
+
+    !!verify_connection(raw_connect(base_url, username, password, verify_ssl))
+  end
+
+  def self.default_api_path
+    "/api/v1".freeze
+  end
+
+  def self.adjust_url(url)
+    url = "https://#{url}" unless url =~ %r{\Ahttps?:\/\/} # HACK: URI can't properly parse a URL with no scheme
+    URI(url).tap do |adjusted_url|
+      adjusted_url.path = default_api_path if adjusted_url.path.blank?
+    end
+  end
+
+  def self.verify_connection(connection)
+    require 'ansible_tower_client'
+    begin
+      connection.api.verify_credentials ||
+        raise(MiqException::MiqInvalidCredentialsError, _("Username or password is not valid"))
+    rescue AnsibleTowerClient::ClientError => err
+      raise MiqException::MiqCommunicationsError, err.message, err.backtrace
+    end
+  end
+
   def self.raw_connect(base_url, username, password, verify_ssl)
     require 'ansible_tower_client'
     AnsibleTowerClient.logger = $ansible_tower_log
@@ -38,27 +120,16 @@ class ManageIQ::Providers::AnsibleTower::Provider < ::Provider
   end
 
   def verify_credentials(auth_type = nil, options = {})
-    require 'ansible_tower_client'
-    begin
-      with_provider_connection(options.merge(:auth_type => auth_type)) { |c| c.api.verify_credentials } ||
-        raise(MiqException::MiqInvalidCredentialsError, _("Username or password is not valid"))
-    rescue AnsibleTowerClient::ClientError => err
-      raise MiqException::MiqCommunicationsError, err.message, err.backtrace
+    with_provider_connection(options.merge(:auth_type => auth_type)) do |c|
+      self.class.verify_connection(c)
     end
   end
 
   def url=(new_url)
-    new_url  = "https://#{new_url}" unless new_url =~ %r{\Ahttps?:\/\/} # HACK: URI can't properly parse a URL with no scheme
-    uri      = URI(new_url)
-    uri.path = default_api_path if uri.path.blank?
-    default_endpoint.url = uri.to_s
+    default_endpoint.url = self.class.adjust_url(new_url).to_s
   end
 
   private
-
-  def default_api_path
-    "/api/v1".freeze
-  end
 
   def ensure_managers
     build_automation_manager unless automation_manager
