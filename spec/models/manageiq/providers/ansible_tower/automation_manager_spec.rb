a676e774c1d3af165c9e2f1be25324560af9a392
Jason Frey <fryguy9@gmail.com>
2020-04-09 13:39:33 -0400

    Merge pull request #217 from agrare/make_catalog_types_pluggable
    Make Provider Catalog Types Pluggable
    
    (cherry picked from commit 512a31594ddd4f133d0af2e9529aa5d2e0027e71)

--- spec/models/manageiq/providers/ansible_tower/automation_manager_spec.rb
+++ spec/models/manageiq/providers/ansible_tower/automation_manager_spec.rb
@@ -1,11 +1,11 @@
 describe ManageIQ::Providers::AnsibleTower::AutomationManager do
   it_behaves_like 'ansible automation_manager'
 
-  context 'catalog types' do
+  describe '#catalog_types' do
     let(:ems) { FactoryBot.create(:automation_manager_ansible_tower) }
 
-    it "#supported_catalog_types" do
-      expect(ems.supported_catalog_types).to eq(%w(generic_ansible_tower))
+    it "#catalog_types" do
+      expect(ems.catalog_types).to include("generic_ansible_tower")
     end
   end
 end
