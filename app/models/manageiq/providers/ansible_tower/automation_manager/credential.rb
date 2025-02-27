e4f0eda14de9d7c885e139cb9045d44aa4497ef0
Joe Rafaniello <jrafanie@gmail.com>
2025-02-27 14:23:16 -0500

    Remove redundant copy/pasted code from awx provider
    The parent class already sets up the manager alias and supports create.
    
    See: https://github.com/ManageIQ/manageiq-providers-awx/blob/6884a085a371dbf0dd28f99f426a0bacac3ff693/app/models/manageiq/providers/awx/automation_manager/credential.rb#L6-L13
    
    This cleans up a rails 7.1 deprecation warning:
    
    DEPRECATION WARNING: ManageIQ::Providers::AnsibleTower::AutomationManager::Credential model aliases `resource`, but `resource` is not an attribute. Starting in Rails 7.2, alias_attribute with non-attribute targets will raise. Use `alias_method :manager, :resource` or define the method manually. (called from new at /home/runner/work/manageiq/manageiq/app/models/mixins/new_with_type_sti_mixin.rb:17)

--- app/models/manageiq/providers/ansible_tower/automation_manager/credential.rb
+++ app/models/manageiq/providers/ansible_tower/automation_manager/credential.rb
@@ -1,14 +1,5 @@
 ManageIQ::Providers::Awx::AutomationManager::Credential.include(ActsAsStiLeafClass)
 
 class ManageIQ::Providers::AnsibleTower::AutomationManager::Credential < ManageIQ::Providers::Awx::AutomationManager::Credential
-  # Authentication is associated with EMS through resource_id/resource_type
-  # Alias is to make the AutomationManager code more uniformly as those
-  # CUD operations in the TowerApi concern
-
-  alias_attribute :manager_id, :resource_id
-  alias_attribute :manager, :resource
-
-  supports :create
-
   FRIENDLY_NAME = 'Ansible Automation Platform Credential'.freeze
 end
